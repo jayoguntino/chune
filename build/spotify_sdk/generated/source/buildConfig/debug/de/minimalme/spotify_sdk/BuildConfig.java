c05f4a9a1c2cc496d4fb5c07f5e3a75c453e5ccb
David Miller <david.miller@accedo.tv>
2021-10-08 12:15:06 +0200

    Added modules for spotify_sdk and added .env to run on emulator

---
+++ build/spotify_sdk/generated/source/buildConfig/debug/de/minimalme/spotify_sdk/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.minimalme.spotify_sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "de.minimalme.spotify_sdk";
+  public static final String BUILD_TYPE = "debug";
+}
